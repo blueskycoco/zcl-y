ad5403a2d947446109b02e5dc1f29c7186293de6
U-F-7F8724A5CE8D4\bbstr <bbstr@f-7f8724a5ce8d4.(none)>
2017-01-17 23:37:00 +0800

    debug ebi

--- bsp/same70/drivers/drv_dsram.c
+++ bsp/same70/drivers/drv_dsram.c
@@ -3,32 +3,101 @@
 
 #include "drv_dsram.h"
 
+const Pin pinsMutex[] = {BOARD_MUTEX_PINS};
+static volatile uint8_t *g_pui8EBISram;
+
 bool dsram_init(void)
 {
 	const Pin pinsDsram[] = {BOARD_DSRAM_PINS};
 	PIO_Configure(pinsDsram, PIO_LISTSIZE(pinsDsram));	
+	PIO_Configure(pinsMutex, PIO_LISTSIZE(pinsMutex));	
 	PMC_EnablePeripheral(ID_SMC);
 
 	/* Configure SMC, NCS2 is assigned to Dual sram */	
-	SMC->SMC_CS_NUMBER[SMC_EBI_DSRAM_CS].SMC_SETUP = 
-						SMC_SETUP_NWE_SETUP(2)      |
-						SMC_SETUP_NCS_WR_SETUP(0)   |			
-						SMC_SETUP_NRD_SETUP(0)      |
-						SMC_SETUP_NCS_RD_SETUP(0);	
-	SMC->SMC_CS_NUMBER[SMC_EBI_DSRAM_CS].SMC_PULSE = 
-						SMC_PULSE_NWE_PULSE(6)	    |		
-						SMC_PULSE_NCS_WR_PULSE(0xA) |			
-						SMC_PULSE_NRD_PULSE(0xA)	|		
-						SMC_PULSE_NCS_RD_PULSE(0xA);	
-	SMC->SMC_CS_NUMBER[SMC_EBI_DSRAM_CS].SMC_CYCLE = 
-						SMC_CYCLE_NWE_CYCLE(0xA)	|		
-						SMC_CYCLE_NRD_CYCLE(0xA);	
+	SMC->SMC_CS_NUMBER[SMC_EBI_DSRAM_CS].SMC_SETUP = 0x01010101;
+	SMC->SMC_CS_NUMBER[SMC_EBI_DSRAM_CS].SMC_PULSE = 0x01010101;
+	SMC->SMC_CS_NUMBER[SMC_EBI_DSRAM_CS].SMC_CYCLE = 0x00030003;
 	SMC->SMC_CS_NUMBER[SMC_EBI_DSRAM_CS].SMC_MODE  = 
 						SMC_MODE_READ_MODE			|
 						SMC_MODE_WRITE_MODE		    |	
-						SMC_MODE_DBW_16_BIT		    |	
-						SMC_MODE_EXNW_MODE_DISABLED |			
-						SMC_MODE_TDF_CYCLES(0xF);
-
+						SMC_MODE_DBW_16_BIT		    |							
+						SMC_MODE_BAT_BYTE_WRITE		|
+						SMC_MODE_TDF_MODE			|
+						SMC_MODE_TDF_CYCLES(0x1);
+	g_pui8EBISram = (uint8_t *)0x60000000;
 	return true;
 }
+
+bool dsram_write(uint8_t addr, uint8_t *data, int len)
+{
+	if (addr+len > DSRAM_LENGTH)
+		return false;
+	//while (PIO_Get(&pinsMutex[1]));
+	PIO_Set(&pinsMutex[0]);
+	rt_memcpy ((void *)(g_pui8EBISram + addr*2), (const void *)data, len);
+	PIO_Clear(&pinsMutex[0]);
+	return true;
+}
+bool dsram_read(uint8_t addr, uint8_t *data, int len)
+{	
+	if (addr+len > DSRAM_LENGTH)
+		return false;
+	//while (PIO_Get(&pinsMutex[1]));
+	PIO_Set(&pinsMutex[0]);
+	rt_memcpy ((void *)data, (const void *)(g_pui8EBISram + addr*2), len);
+	PIO_Clear(&pinsMutex[0]);
+	return true;
+}
+#ifdef RT_USING_FINSH
+#include <finsh.h>
+static uint8_t data[DSRAM_LENGTH]={0};
+static void dsram_init_t(void)
+{
+	bool result = dsram_init();
+	if (result == true)
+		rt_kprintf("dsram_init ok\n");
+	else		
+		rt_kprintf("dsram_init failed\n");
+}
+static void dsram_read_t(int addr, int len)	
+{
+	bool result = dsram_read(addr,data, len);
+	if (result == true)
+	{
+		int i;
+		rt_kprintf("dsram_read %d :\n", len);
+		for (i = 0; i < len; i++)
+		{
+			rt_kprintf("%02x ", data[i]);
+			if ((i+1)%8 == 0)
+				rt_kprintf("\n");
+		}
+		rt_kprintf("\n");
+	}
+	else		
+		rt_kprintf("dsram_read failed\n");
+}
+static void dsram_write_t(int addr, int begin, int len)	
+{
+	int i;
+	rt_kprintf("dsram_write %d :\n", len);
+	for (i = 0; i < len; i++)
+	{
+		data[i] = begin+i;
+		rt_kprintf("%02x ", data[i]);
+		if ((i+1)%8 == 0)
+			rt_kprintf("\n");
+	}
+	rt_kprintf("\n");
+	bool result = dsram_write(addr,data, len);
+	if (result == true)
+		rt_kprintf("dsram_write ok\n");
+	else		
+		rt_kprintf("dsram_write failed\n");
+}
+
+FINSH_FUNCTION_EXPORT(dsram_init_t, test dsram_init);
+FINSH_FUNCTION_EXPORT(dsram_write_t, test dsram_write);
+FINSH_FUNCTION_EXPORT(dsram_read_t, test dsram_read);
+#endif
+
--- bsp/same70/drivers/drv_dsram.h
+++ bsp/same70/drivers/drv_dsram.h
@@ -18,15 +18,20 @@
 /*  Dual port sram CS pin (NCS2) */
 #define PIN_EBI_DSRAM_CS          {PIO_PA22C_NCS2, PIOA, ID_PIOA, PIO_PERIPH_C, PIO_PULLUP}
 /** Dual port sram WR pin */
-#define PIN_EBI_DSRAM_NWR         {PIO_PD15C_NWR1,  PIOD, ID_PIOD, PIO_PERIPH_C, PIO_PULLUP}
+#define PIN_EBI_DSRAM_NWR         {PIO_PD15C_NBS1,  PIOD, ID_PIOD, PIO_PERIPH_C, PIO_PULLUP}
 /** Dual port sram BS pin */
 #define PIN_EBI_DSRAM_NBS         {PIO_PC18A_NBS0,  PIOC, ID_PIOC, PIO_PERIPH_A, PIO_PULLUP}
 
 #define BOARD_DSRAM_PINS PIN_SDRAM_D0_7, PIN_EBI_DSRAM_D8_13 , PIN_EBI_DSRAM_D14_15,\
 	PIN_EBI_DSRAM_A1_12, PIN_EBI_DSRAM_NWE, PIN_EBI_DSRAM_NRD, \
 	PIN_EBI_DSRAM_CS, PIN_EBI_DSRAM_NWR, PIN_EBI_DSRAM_NBS
-#define SMC_EBI_DSRAM_CS          2
 
+#define PIN_ARM2DSP	{PIO_PD0, PIOD, ID_PIOD, PIO_OUTPUT_0, PIO_DEFAULT}
+#define PIN_DSP2ARM	{PIO_PD27, PIOD, ID_PIOD, PIO_INPUT, PIO_DEFAULT}
+#define BOARD_MUTEX_PINS PIN_ARM2DSP, PIN_DSP2ARM
+#define SMC_EBI_DSRAM_CS          2
+#define DSRAM_BASE				  0x62000000
+#define DSRAM_LENGTH			  8192
 bool dsram_init(void);
 bool dsram_write(uint8_t addr, uint8_t *data, int len);
 bool dsram_read(uint8_t addr, uint8_t *data, int len);
